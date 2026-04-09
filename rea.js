159ba5e9f7e3c40173444bba0a30c45c73440c22
hi <Ggookkuull@gmail.com>
2026-04-09 18:20:11 +0530

    rea.js

---
+++ rea.js
@@ -0,0 +1,31 @@
+// let a = [10, 20, 30];
+// console.log(a);
+// document.write("Values : ",a)
+
+// let ans = a.forEach((i, j) => {
+//   i += 10;
+//   return i;
+// });
+
+// let an = a.map((i, j) => {
+//   i += 10;
+//   return i;
+// });
+
+// console.log(an);
+
+// let ans = a.some((v)=>v%2==0)
+// let ans = a.every((v)=>v%2==0)
+// console.log(ans);
+
+// function add(i,j){
+//     return i+j
+// }
+// let a = add(10,10)
+// console.log(a);
+
+// add = (i,j)=>{
+//     return i+j
+// }
+// let a = add(10,10)
+// console.log(a);
